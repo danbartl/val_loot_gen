657de576815f95b3763f94b5c7d26bc6c7ed7233
dbart79 <dampfschlaghammer@gmail.com>
2021-05-08 23:21:36 +0200

    initial2

--- R/master.R
+++ R/master.R
@@ -1,34 +1,6 @@
 
-#Specify the ratio of conversion
-conversion_enchantmats <- 5
-#from lowest to highest rarity across four tiers
-tiers <- 4
-
-#Drops are scaled linearly from weakest to strongest creeature
-expected_drops <- c(0.2,10)
-
-#How many legendaries should you get for highest difficulty mob
-legendary_count <- 8
-
-#Which hero scenarios should be relevant?
-#early, mid, end
-hero_weight <- c(0.25,0.25,0.5)
-
-early_hero_armor <- 20
-early_hero_damage <- 15
-early_hero_hp <- 90
-
-mid_hero_armor <- 80
-mid_hero_damage <- 60
-mid_hero_hp <- 140
-
-
-end_hero_armor <- 200
-end_hero_damage <- 140
-end_hero_hp <- 460
-
-
 source("R/load_lib_func.R")
+source("R/set_parameters.R")
 source("R/read_creaturedata.R")
 source("R/read_cllc_yaml.R")
 source("R/add_levelupdata.R")
